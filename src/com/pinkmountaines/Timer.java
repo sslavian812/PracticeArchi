9d6170a98d3580d6ade021c09294bb39c86aed78
Ruslan <akhundov2@gmail.com>
2015-11-07 11:22:20 +0300

    additional task done

--- src/com/pinkmountaines/Timer.java
+++ src/com/pinkmountaines/Timer.java
@@ -1,8 +1,14 @@
 package com.pinkmountaines;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.logging.Logger;
 
 public class Timer implements Runnable {
+    private static final Logger LOG = Logger.getLogger(Timer.class.getName());
+    private static final String LAST_SAVED_TIME_FILE_NAME = "res/lastUpdateTime";
+
+    private long initTime;
 
     private boolean running = false;
     private boolean paused = false;
@@ -11,6 +17,20 @@ public class Timer implements Runnable {
 
     public Timer(TimeFrame timeFrame) {
         this.timeFrame = timeFrame;
+        getInitTime();
+    }
+
+    private void getInitTime() {
+        try {
+            FileReader reader = new FileReader(LAST_SAVED_TIME_FILE_NAME);
+            BufferedReader bf = new BufferedReader(reader);
+            long savedCurrentTime = Long.parseLong(bf.readLine());
+            long lastTimerValue = Long.parseLong(bf.readLine());
+            this.summedTime = System.currentTimeMillis() - savedCurrentTime + lastTimerValue;
+        } catch (IOException | NumberFormatException e) {
+            this.initTime = 0;
+            LOG.severe("Couldn't read file " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -39,6 +59,16 @@ public class Timer implements Runnable {
         long startTime = System.currentTimeMillis();
         while (running && !paused) {
             timeFrame.update(summedTime + (System.currentTimeMillis() - startTime));
+
+            try {
+                PrintWriter writer = new PrintWriter(LAST_SAVED_TIME_FILE_NAME);
+                writer.println(System.currentTimeMillis());
+                writer.println(summedTime);
+                writer.close();
+            } catch (IOException e) {
+                LOG.severe("Couldn't save time" + e.getMessage());
+            }
+
         }
         if (paused) {
             summedTime += System.currentTimeMillis() - startTime;
